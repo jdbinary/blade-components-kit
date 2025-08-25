bb74e5dad461a4ccf5102d94ae8f11003cec3bb8
Johney Diaz <131538866+jdbinary@users.noreply.github.com>
2025-08-25 13:44:48 -0500

    Update card.blade.php

--- resources/views/components/layouts/auth/card.blade.php
+++ resources/views/components/layouts/auth/card.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
-
-<head>
-    @include('partials.head')
-</head>
-
-<body class="min-h-screen bg-primary-100 antialiased dark:bg-linear-to-b dark:from-primary-950 dark:to-primary-900">
+<x-layouts.layout>
     <div class="absolute right-6 top-1">
         <x-toggle icons="true" color="primary" shape="circle" />
     </div>
@@ -13,7 +6,7 @@
         <div class="flex w-full max-w-md flex-col gap-6">
             <x-link href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
                 <span class="flex items-center justify-center text-primary-950 dark:text-primary-100">
-                    <x-icon name="logo-laravel" size="10" />
+                    <img src="{{ asset('img/blade.svg') }}" alt="Blade Custom Kit Logo" width="256">
                 </span>
 
                 <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
@@ -26,8 +19,4 @@
             </div>
         </div>
     </div>
-    @fluxScripts
-</body>
-
-
-</html>
+</x-layouts.layout>
