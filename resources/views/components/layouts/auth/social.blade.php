4c51c4996357e938ee8c1ad0cdd7a662056ab8ff
Johney Diaz <131538866+jdbinary@users.noreply.github.com>
2025-08-25 13:44:12 -0500

    Update social.blade.php

--- resources/views/components/layouts/auth/social.blade.php
+++ resources/views/components/layouts/auth/social.blade.php
@@ -1,34 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-
-<head>
-    @include('partials.head')
-</head>
-
-<body class="min-h-screen bg-primary-50 antialiased dark:bg-linear-to-b dark:from-primary-950 dark:to-primary-900">
-
+<x-layouts.layout>
     <div class="flex min-h-screen">
         <div class="flex-1 p-4 max-lg:hidden">
             <div class="relative rounded-lg h-full w-full bg-primary-100 dark:bg-primary-900 flex flex-col items-start justify-end p-16"
                 style="background-image: url('/img/auth/image-card.jpg'); background-size: cover">
-                <div class="flex gap-2 mb-4">
-                    <x-icon name="star" />
-                    <x-icon name="star" />
-                    <x-icon name="star" />
-                    <x-icon name="star" />
-                    <x-icon name="star" />
-                </div>
 
-                <div class="mb-6 italic font-base text-3xl xl:text-4xl">
-                    Flux has enabled me to design, build, and deliver apps faster than ever before.
-                </div>
-
-                <div class="flex gap-4">
-                    <div class="flex flex-col justify-center font-medium">
-                        <div class="text-lg">Caleb Porzio</div>
-                        <div class="text-zinc-300">Creator of Livewire</div>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="absolute right-6 top-2">
@@ -42,7 +17,7 @@
                         class="relative z-20 flex items-center text-lg font-medium text-primary-950 dark:text-primary-100"
                         wire:navigate>
                         <span class="flex items-center justify-center rounded-md ">
-                            <img src="{{ asset('img/blade.webp') }}" alt="">
+                            <img src="{{ asset('img/blade.webp') }}" alt="Blade Custom Kit Logo" width="256">
                         </span>
 
                     </a>
@@ -74,6 +49,4 @@
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</x-layouts.layout>
